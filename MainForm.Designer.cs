9398cb49804e4b5ba27fbb30d41f66b3943ce1e2
sibiu <144681144+razvantrufin99xx@users.noreply.github.com>
2024-09-18 18:25:12 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 9/18/2024
+ * Time: 5:47 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace sirMergator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(23, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(1247, 148);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(45, 468);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(106, 39);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(167, 393);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(106, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "stop";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(45, 393);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(106, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "start";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(291, 393);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(106, 23);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "pauza";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(414, 393);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(106, 23);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "restart";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1282, 519);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "sirMergator";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 9/18/2024
+ * Time: 5:47 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace sirMergator
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			loadtext();
+		}
+		
+		public string s = "Reclama la detergenti : Exte ora 16 si un sfert ";
+		public int mode = 0;
+		public int tcounter = 0;
+		public void loadtext()
+		{
+			label1.Width = 400;
+			label1.Text = s;
+			
+			
+		}
+		public void run()
+		{
+			int r = 0;
+			int counter = 0 ;
+			//while(r == 0 && counter<this.Width-label1.Width)
+			//{
+				counter+=1;
+				if(mode==0) {toRight();}
+				if(mode==1){toLeft();}
+				if(counter==this.Width-label1.Width){counter=0;r=1;}
+				//Thread.Sleep(1);
+			//};
+		}
+		public void toRight()
+		{
+			if(mode==0){
+			if(label1.Left<this.Width-label1.Width){
+				label1.Left++;
+			}
+			else 
+			{
+				mode = 1;
+			}
+			}
+			
+			
+		}
+		
+		public void toLeft()
+		{
+			if(mode==1){
+			if(label1.Left>0){
+				label1.Left--;
+			}
+			else 
+			{
+				mode = 0;
+			}
+			}
+			
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			loadtext();
+				
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			tcounter++;
+			run();	
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			mode=-2;
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			mode=-1;
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+			mode=0;
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			mode=-3;
+		}
+	}
+}
